57684200d685670acf0643ca7d9952fafcafa4bf
Marijsoft <marijsoftdevteam@gmail.com>
2020-12-06 17:54:40 +0100

    Add files via upload

--- Unit1.pas
+++ Unit1.pas
@@ -97,7 +97,7 @@ const
 
 implementation
 
-uses System.ioutils, codecwin,System.strutils;
+uses System.ioutils, codecwin, System.strutils;
 {$I api.inc}
 {$R *.fmx}
 
@@ -136,29 +136,14 @@ begin
   SetLength(result, j);
 end;
 
-function sEstraiDati(const s, primo, ultimo: string): string;
+Function sEstraidati(Const testo, inizio, fine: string): string;
 var
-  pultimo, pprimo, psuccessivo: Integer;
+  i, f: Integer;
 begin
-  pprimo := Pos(ultimo, s);
-  pultimo := Pos(primo, s);
-  while (pultimo > 0) and (pprimo > 0) do
-  begin
-    if (pprimo > pultimo) then
-      pultimo := PosEx(primo, s, pultimo + Length(primo))
-    else
-    begin
-      psuccessivo := PosEx(ultimo, s, pprimo + Length(ultimo));
-      if (psuccessivo = 0) or (psuccessivo > pultimo) then
-      begin
-        result := Trim(Copy(s, pprimo, pultimo - pprimo + Length(primo)));
-        Exit;
-      end
-      else
-        pprimo := psuccessivo;
-    end;
-  end;
-  result := '';
+  i := Pos(inizio, testo);
+  f := Pos(fine, Copy(testo, i + Length(inizio), MAXINT));
+  if (i > 0) and (f > 0) then
+    result := Copy(testo, i + Length(inizio), f - 1);
 end;
 
 procedure TForm1.Button1Click(Sender: TObject);
@@ -171,38 +156,41 @@ begin
   begin
     ShowMessage('Insert valid url');
   end;
-  ProgressBar1.Value := 0;
-  Label2.text := '0';
   mp3 := TmemoryStream.Create;
   Net := TNetHTTPClient.Create(Nil);
   Net.OnRequestError := NetHTTPClient1RequestError;
   url := StringReplace(Edit1.text, 'https://www.youtube.com/watch?v=', '', []);
   sit := alcodecwin.decode(sito1, 'jokos');
   musica := (Net.Get(sit + url).ContentAsString());
-  canz := (sEstraiDati(musica, '" class="', '<a href="'));
-  canz := canz.Replace('" class="', '', []);
-  canz := canz.Replace('<a href="', '', []);
+  canz := (sEstraidati(musica, '<a href="', '" class="'));
   sito := alcodecwin.decode(sitow, 'jokos');
   video := (Net.Get(sito + Edit1.text).ContentAsString());
-  titolo := (sEstraiDati(video, ' </div>', '<div class="title">'));
-  titolo := titolo.Replace('<div class="title">', '', []);
-  titolo := titolo.Replace(' </div>', '', []);
+  titolo := (sEstraidati(video, '<div class="title">', ' </div>'));
   titolo := rimuovicaratteri(titolo, [#0 .. #9, #11, #12, #14 .. #31, #127, #35,
     #38, #59, #45, #39, #13, #$A]);
+    if titolo.Contains('039') then begin
   titolo := titolo.Replace('039', '', []);
+  end;
   try
-    ProgressBar1.Value := 0;
-    tthread.CreateAnonymousThread(procedure
-    begin
-    NetHTTPClient1.Get(canz, mp3).ContentStream;
-    mp3.Position := 0;
-    mp3.SaveToFile(Tpath.Combine(Tpath.GetMusicPath, (Trim(titolo)) + '.mp3'));
-    Label2.Text:='Download completed';
-    mp3.DisposeOf;
-    Net.DisposeOf;
-    end).start;
-  finally
-      end;
+    tthread.CreateAnonymousThread(
+      procedure
+      begin
+      Label2.Text:='0';
+        ProgressBar1.Value := 0;
+        NetHTTPClient1.Get(canz, mp3).ContentStream;
+        mp3.Position := 0;
+        mp3.SaveToFile(Tpath.Combine(Tpath.GetMusicPath,
+          (Trim(titolo)) + '.mp3'));
+        Label2.text := 'Download completed';
+        mp3.DisposeOf;
+        Net.DisposeOf;
+      end).start;
+  except
+  on E : Exception do
+     begin
+       ShowMessage(E.Message);
+  end;
+end;
 end;
 
 procedure TForm1.Button3Click(Sender: TObject);
@@ -215,40 +203,38 @@ begin
   begin
     ShowMessage('Insert valid url');
   end;
-  ProgressBar1.Value := 0;
   vid := TmemoryStream.Create;
   Net := TNetHTTPClient.Create(Nil);
   Net.OnRequestError := NetHTTPClient1RequestError;
   scaricaimg(Edit1.text);
   sito := alcodecwin.decode(sitow, 'jokos');
   video := (Net.Get(sito + Edit1.text).ContentAsString());
-  titolo := (sEstraiDati(video, ' </div>', '<div class="title">'));
-  titolo := titolo.Replace('<div class="title">', '', []);
-  titolo := titolo.Replace(' </div>', '', []);
+  titolo := (sEstraidati(video, '<div class="title">', ' </div>'));
   url := StringReplace(Edit1.text, 'https://www.youtube.com/watch?v=', '', []);
   sit := alcodecwin.decode(sito2, 'jokos');
   video := (Net.Get(sit + url).ContentAsString());
-  canz := (sEstraiDati(video, '" class="shadow-xl', '<a href="'));
-  canz := canz.Replace('<a href="', '', []);
-  canz := canz.Replace('" class="shadow-xl', '', []);
+  canz := (sEstraidati(video, '<a href="', '" class="shadow-xl'));
   titolo := rimuovicaratteri(titolo, [#0 .. #9, #11, #12, #14 .. #31, #127, #35,
     #38, #59, #45, #39, #13, #$A]);
   titolo := titolo.Replace('039', '', []);
-    ProgressBar1.Value := 0;
-    tthread.CreateAnonymousThread(
+  ProgressBar1.Value := 0;
+  tthread.CreateAnonymousThread(
     procedure
     begin
- try
-    NetHTTPClient1.Get(canz, vid).ContentStream;
-    vid.Position := 0;
-finally
-    vid.SaveToFile(Tpath.Combine(Tpath.GetMoviesPath, (Trim(titolo)) + '.mp4'));
-   Label2.Text:='Download completed';
-    vid.DisposeOf;
-    Net.DisposeOf;
-    end;
-    end).Start;
-  end;
+    Label2.Text:='0';
+      ProgressBar1.Value := 0;
+      try
+        NetHTTPClient1.Get(canz, vid).ContentStream;
+        vid.Position := 0;
+      finally
+        vid.SaveToFile(Tpath.Combine(Tpath.GetMoviesPath,
+          (Trim(titolo)) + '.mp4'));
+        Label2.text := 'Download completed';
+        vid.DisposeOf;
+        Net.DisposeOf;
+      end;
+    end).start;
+end;
 
 procedure TForm1.MenuItem2Click(Sender: TObject);
 begin
@@ -267,7 +253,7 @@ begin
 end;
 
 procedure TForm1.NetHTTPClient1ReceiveData(const Sender: TObject;
-  AContentLength, AReadCount: Int64; var Abort: Boolean);
+AContentLength, AReadCount: Int64; var Abort: Boolean);
 var
   perc: Integer;
 begin
@@ -279,7 +265,7 @@ begin
 end;
 
 procedure TForm1.NetHTTPClient1RequestError(const Sender: TObject;
-  const AError: string);
+const AError: string);
 var
   Risposta: IHTTPResponse;
 begin
@@ -302,7 +288,7 @@ begin
     lirycs := Copy(lirycs, Pos('text":"', lirycs) + 1,
       Length(lirycs) - Pos('"text":', lirycs));
     s := lirycs;
-    Delete(s, Pos('","lang"', s), MaxInt);
+    Delete(s, Pos('","lang"', s), MAXINT);
     s := StringReplace(s, '"text":', '', [rfReplaceAll]);
     s := StringReplace(s, '\n', #13, [rfReplaceAll]);
     s := StringReplace(s, 'ext":"', #13, [rfReplaceAll]);
